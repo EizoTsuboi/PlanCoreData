34919a012e20f4e9638dcc7f3b2e746b9d5fc461
EizoTsuboi <eizo.wakuwaku@gmail.com>
2019-08-30 10:13:14 +0900

    Initial Commit

---
+++ PlanCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlanCoreData
+//
+//  Created by 坪井衛三 on 2019/08/30.
+//  Copyright © 2019 Eizo Tsuboi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
