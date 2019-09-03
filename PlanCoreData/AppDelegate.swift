157895104f9f0ab7bfa17be9010a6957051a8960
EizoTsuboi <eizo.wakuwaku@gmail.com>
2019-09-03 20:16:14 +0900

    add initial sqlite

--- PlanCoreData/AppDelegate.swift
+++ PlanCoreData/AppDelegate.swift
@@ -54,13 +54,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
+        
         let container = NSPersistentContainer(name: "PlanCoreData")
+        
+        var persistentStoreDescriptions: NSPersistentStoreDescription
+        
+        // Documentsディレクトリのパスを取得
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        // sqliteファイルが置かれるURL
+        let storeUrl = documentsDirectory.appendingPathComponent("PlanCoreData.sqlite")
+        
+        // そのURLにファイルが存在しない(=基本は初期化時)時、
+        // 事前に作成したsqliteファイルをそこにコピーする
+        if !FileManager.default.fileExists(atPath: (storeUrl.path)) {
+            let seededDataUrl = Bundle.main.url(forResource: "PlanCoreData", withExtension: "sqlite")
+            try! FileManager.default.copyItem(at: seededDataUrl!, to: storeUrl)
+        }
+        
+        // 各種設定
+        let description = NSPersistentStoreDescription()
+        description.shouldInferMappingModelAutomatically = true
+        description.shouldMigrateStoreAutomatically = true
+        description.url = storeUrl
+        
+        container.persistentStoreDescriptions = [description]
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -69,7 +94,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                  * The store could not be migrated to the current model version.
                  Check the error message to determine what the actual problem was.
                  */
-                fatalError("ERRORRECORD1:  \(error), \(error.userInfo)")
+                fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         return container
